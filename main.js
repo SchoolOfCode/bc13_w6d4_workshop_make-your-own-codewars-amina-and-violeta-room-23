a11091340cb8eb94fb95ef23f9d9e96c39b46f96
edmundsamina <edmundsamina@gmail.com>
2022-11-03 15:14:45 +0000

    basic second kata done

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-/* 
-👉 Write your kata here!
 
-*/
-
+//KATA - 1 (ADD TWO POSITIVE INTEGERS)
 export function addNumbers(a, b) {
     //Write a function that add a and b, returns the result
     //a and b will always be positive integers
-    return a + b;
+    return a + b
+
+};
+
+
+//KATA 2 - (FIND THE AVERAGE): 
+//You work at a shop and are trying to find the average profit for your shop for the week
+//Write a function that takes in the daily sales for the shop, which can be either a positive
+//or negative integer.
+
+
+export function weeklyAverageProfit(Mon, Tues, Wed, Thurs, Fri, Sat, Sun){
+ let averageSales = (Mon + Tues + Wed + Thurs + Fri + Sat + Sun)/7
+ return averageSales
 }
-
-//👉 Write the function your CodeWarriors will start with below here:
-
--- main.test.js
+++ main.test.js
@@ -1,17 +1,51 @@
 //👉 Write your tests below here:
 
-import { addNumbers } from "./main.js"
+import { addNumbers, weeklyAverageProfit } from "./main.js"
 import { test, expect } from "@jest/globals"
 
-test('a + b should be positive integer', () => {
-    let randomNumber = Math.ceil(Math.random()*5)
-    let a = randomNumber
-    let b = randomNumber
-    let c = a + b
+let ones = Math.ceil(Math.random()*5)
+let tens = Math.ceil(Math.random()*10)
+let hundreds = Math.ceil(Math.random()*100)
+let thousands = Math.ceil(Math.random()*10000)
+
+//TEST FOR FIRST KATA
+test.each([
+  [ones, ones],
+  [tens, tens],
+  [hundreds, hundreds],
+  [hundreds, tens],
+  [ones, tens],
+  [hundreds, ones],
+  [thousands, hundreds],
+  [thousands, ones],
+  [thousands, tens],
+  [thousands, thousands],
+])('Checks that the numbers are added correctly', (a, b) => {
     const actual = addNumbers (a, b);
-    const expected = c;
-    console.log(a, b, c, actual)
+    const expected = a + b;
     expect(actual).toBe(expected);
-  });
+});
+
+//TEST FOR KATA TWO 
+test.each([
+  [ones, ones, thousands, hundreds, hundreds, hundreds, hundreds],
+])('Calculates the correct average', (Mon, Tues, Wed, Thurs, Fri, Sat, Sun) => {
+    const actual = weeklyAverageProfit(Mon, Tues, Wed, Thurs, Fri, Sat, Sun);
+    const expected = (Mon + Tues + Wed + Thurs + Fri + Sat + Sun)/7;
+    console.log(Mon, Tues, Wed, Thurs, Fri, Sat, Sun)
+    console.log(expected, actual)
+    expect(actual).toBe(expected);
+});
+
+// test('a + b should be positive integer', () => {
+//     let randomNumber = Math.ceil(Math.random()*5)
+//     let a = randomNumber
+//     let b = randomNumber
+//     let c = a + b
+//     const actual = addNumbers (a, b);
+//     const expected = c;
+//     console.log(a, b, c, actual)
+//     expect(actual).toBe(expected);
+//   });
 
 
