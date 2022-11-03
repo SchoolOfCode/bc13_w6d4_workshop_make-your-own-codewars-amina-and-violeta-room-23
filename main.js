fb05ab1299ad4297192ff2503f1c0d2f894adab8
violetacf <violeta@cique.es>
2022-11-03 14:25:10 +0000

    basic test done

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 /* 
 👉 Write your kata here!
+
 */
 
+export function addNumbers(a, b) {
+    //Write a function that add a and b, returns the result
+    //a and b will always be positive integers
+    return a + b;
+}
+
 //👉 Write the function your CodeWarriors will start with below here:
+
--- main.test.js
+++ main.test.js
@@ -1,1 +1,17 @@
 //👉 Write your tests below here:
+
+import { addNumbers } from "./main.js"
+import { test, expect } from "@jest/globals"
+
+test('a + b should be positive integer', () => {
+    let randomNumber = Math.ceil(Math.random()*5)
+    let a = randomNumber
+    let b = randomNumber
+    let c = a + b
+    const actual = addNumbers (a, b);
+    const expected = c;
+    console.log(a, b, c, actual)
+    expect(actual).toBe(expected);
+  });
+
+
